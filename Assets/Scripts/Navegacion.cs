696e48b83727b91d59e67b1837b3b565040f8aca
Camilo Armenta <camiloarmenta285>
2023-09-29 09:12:47 -0500

    Agregando script

--- Assets/Scripts/Navegacion.cs
+++ Assets/Scripts/Navegacion.cs
@@ -1,18 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using MoreMountains.Tools;
 
 public class Navegacion : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
+    public void Play()
+    {
+        Debug.Log("Preiono play");
+        //MMSceneLoadingManager.LoadScene("KoalaDungeon");
+    }
+
+    public void Instrucciones()
+    {
+        Debug.Log("Preiono instruccionesS");
+    }
+
+    public void Ajustes()
+    {
+        Debug.Log("Preiono Ajustes");
+    }
+
+
+    public void Creditos()
+    {
+        Debug.Log("Preiono Creditos");
+    }
+
+    public void Salir()
+    {
+        Debug.Log("Preiono Salir");
+    } 
 }
+
+
