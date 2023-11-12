dd3e75122661b071dcdfb01239662aaa68d371b5
Camilo Armenta <camiloarmenta285>
2023-11-12 15:03:25 -0500

    C0nfigurando Unity

--- Assets/Scripts/Torre.cs
+++ Assets/Scripts/Torre.cs
@@ -1,10 +1,13 @@
+using MoreMountains.TopDownEngine;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 public class Torre : MonoBehaviour
 {
-
+    [SerializeField] Slider healthSlider;
+    [SerializeField] int sceneToLoad;
     public int startingHealth = 1000;
 
     int currentHealth;
@@ -20,13 +23,14 @@ public class Torre : MonoBehaviour
     {
         if (!isAlive) return;
         currentHealth -= damage;
-
+        healthSlider.value = Mathf.Max(currentHealth, 0);
         Debug.Log($"Recibi daño dmg:{damage} - current:{currentHealth}");
         if(currentHealth <= 0) 
         {
             isAlive = false;
             Debug.Log("Se murio");
-        //se murio hace algo
+            //se murio hace algo
+            SceneManager.LoadScene(sceneToLoad);
         }
     }
 }
