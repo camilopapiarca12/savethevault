2f58f11c2546f01c4e3122839ba45be31cfa527b
Camilo Armenta <145043400+camilopapiarca12@users.noreply.github.com>
2023-11-16 00:28:31 -0500

    Todo OK

--- Assets/Scripts/BuildManager.cs
+++ Assets/Scripts/BuildManager.cs
@@ -1,1 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
+public class BuildManager : MonoBehaviour
+{
+    public static BuildManager main;
+
+    [Header("References")]
+    //[SerializeField] private GameObject[] towerPrefabs;
+    [SerializeField] private Tower[] towers;
+
+    private int SelectedTower = 0;
+
+        private void Awake()
+        {
+            main = this;
+        }
+
+    public Tower GetSelectedTower() 
+    {
+        return towers[SelectedTower];
+    }
+
+    public void SetSelectedTower(int _selectedTower)
+    {
+        SelectedTower = _selectedTower;
+    }
+
+}
